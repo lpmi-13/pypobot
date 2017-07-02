e22b790438204a6e820da79170981f613b5cba62
lpmi-13 <leskis@gmail.com>
2017-07-02 15:48:25 +0100

    add extra import to check for different aux verbs and helper functions to automate db updates

--- fileparser.py
+++ fileparser.py
@@ -38,7 +38,7 @@ def parsefile(original_array, aux_list, form, correction_form):
                 print 'found one'
                 suggestion = line.replace(phrase, suggestion_phrase, len(found_at))
                 user_input = raw_input('would you like to change: \n\n {} \n\n into \n\n {} ...?\n\n (y/n)'.format(line, suggestion))
-                if (user_input is 'y' or 'Y'):
+                if (user_input is 'y'):
                     correction_array[current_index] = suggestion
                 else:
                     correction_array[current_index] = line
