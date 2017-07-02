9fd3c64bc1efabcd2e22f75a10bf454899237233
lpmi-13 <leskis@gmail.com>
2017-07-02 15:46:05 +0100

    update handling of database to improve visibility of submitted/merged PRs

--- prompt.py
+++ prompt.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import git, os, shutil, requests, time
 import yaml,json
 from fileparser import parsefile
+from update_db import clear_already_merged
 
 from loggerPrefs import prefs
 
@@ -13,8 +14,8 @@ be_aux_forms = [
 ]
 
 #these are hardcoded for now but will eventually be procedurally generated
-FORM = 'spend'
-CORRECTION_FORM = 'spent'
+FORM = 'send'
+CORRECTION_FORM = 'sent'
 
 with open("settings.yaml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
@@ -23,115 +24,131 @@ db = MongoClient().pypo
 g = Github(cfg['user'], cfg['password'], timeout=200, per_page=30)
 user = g.get_user()
 
+
+print 'checking for merged PRs and updating DB...'
+
+clear_already_merged(g, db)
+
+import pdb;pdb.set_trace()
+
 response = raw_input('Would you like to see the first suggested correction? (y/n)')
 
 
 if (response is 'y'):
     documents = db.pypo.find()
     for item in documents:
-        original = item['line']
-        suggestion = item['line'].replace(' {}'.format(FORM), ' {}'.format(CORRECTION_FORM))
-        print '-------------------- original:\n',item['line'],'\n\n--------------- suggested fix:\n ', suggestion,'\n\n'
-        response = raw_input('Would you like to correct this? (y/n)')
-        if (response is 'y' or 'Y'):
-            print 'grabbing repo at %s ...'%(item['url'])
-            result = g.get_repo(item['repo_id'])
-            repo_name = result.full_name
-            repo_owner = result.owner.login
 
-            print 'creating remote fork for %s ...'%(item['url'])
-
-            forked = user.create_fork(result)
-
-            DIR_ROOT = 'forks/'
-            FULL_PATH = DIR_ROOT + repo_name
-            REMOTE_URL = forked.ssh_url
-
-            if os.path.isdir(FULL_PATH):
-                shutil.rmtree(FULL_PATH)
-            os.makedirs(FULL_PATH)
-
-            local_repo = git.Repo.init(FULL_PATH)
-            origin = local_repo.create_remote('origin', REMOTE_URL)
-
-            print 'cloning local copy of fork in %s'%(FULL_PATH)
-
-            try:
-                origin.fetch()
-                origin.pull(origin.refs[0].remote_head)
-
-
-                indexOfRepoName = local_repo.working_dir.rfind('/')
-                local_repo_name = local_repo.working_dir[indexOfRepoName:]
-
-                new_branch_name = 'typofix'
-                print 'creating new feature branch %s for %s'%(new_branch_name, FULL_PATH)
-                new_branch = local_repo.create_head(new_branch_name)
-                local_repo.heads.typofix.checkout()
-
-                #this is just because README files can have random file names/extensions
-                api_response = requests.get('https://api.github.com/repos/' + repo_name + '/readme')
-
-
-                repoInfo = api_response.json()
-
-                readmeFile = repoInfo['name']
-
-                pathToReadmeFile = FULL_PATH + '/' + readmeFile
-
-                print 'found readme at {}'.format(pathToReadmeFile)
-
-                newFileArray = []
-
+        # checks if this particular typo already has a pull request submitted. If so, skipped.
+        if (item['pull_request_submitted'] == True):
+            continue
+        else:
+    
+            original = item['line']
+            suggestion = item['line'].replace(' {}'.format(FORM), ' {}'.format(CORRECTION_FORM))
+            print 'looking at {}\n\n'.format(item['url'])
+            print '-------------------- original:\n',item['line'],'\n\n--------------- suggested fix:\n ', suggestion,'\n\n'
+            response = raw_input('Would you like to correct this? (y/n)')
+            if (response is 'y'):
+                print 'grabbing repo at %s ...'%(item['url'])
+                result = g.get_repo(item['repo_id'])
+                repo_name = result.full_name
+                repo_owner = result.owner.login
+                default_branch = result.default_branch
+    
+                print 'creating remote fork for %s ...'%(item['url'])
+    
+                forked = user.create_fork(result)
+    
+                DIR_ROOT = 'forks/'
+                FULL_PATH = DIR_ROOT + repo_name
+                REMOTE_URL = forked.ssh_url
+    
+                if os.path.isdir(FULL_PATH):
+                    shutil.rmtree(FULL_PATH)
+                os.makedirs(FULL_PATH)
+    
+                local_repo = git.Repo.init(FULL_PATH)
+                origin = local_repo.create_remote('origin', REMOTE_URL)
+    
+                print 'cloning local copy of fork in %s'%(FULL_PATH)
+    
                 try:
-                    with open(pathToReadmeFile) as readme:
-                        for line in readme:
-                            newFileArray.append(line)
-                    #this is where the loop through all auxiliary forms happens
-                    revised_array = parsefile(newFileArray, be_aux_forms, FORM, CORRECTION_FORM)
-
-                    #this takes the full updated readme and writes it to the file
-                    with open(pathToReadmeFile, 'w') as new_readme:
-                        for line in revised_array:
-                            new_readme.write(line)
-
-                    print 'adding new README and committing to the ' + new_branch_name + ' branch'
-
-                    updatedReadmePath = os.path.join(local_repo.working_tree_dir, readmeFile)
-
-                    local_repo.index.add([updatedReadmePath])
-                    local_repo.index.commit('fix simple typo')
-
-
-                    print 'pushing back changes to remote fork'
-                    try:
+                    origin.fetch()
+                    origin.pull(refspec='{}'.format(default_branch))
     
-                        origin.push(refspec='{}'.format(new_branch_name))
-                    except:
+                    indexOfRepoName = local_repo.working_dir.rfind('/')
+                    local_repo_name = local_repo.working_dir[indexOfRepoName:]
     
-                        local_repo.remotes.origin.pull(refspec='master')
-                        origin.push(refspec='{}'.format(new_branch_name))
-    #                '''
-                    print 'creating pull request from new branch'
+                    new_branch_name = 'typofix'
+                    print 'creating new feature branch %s for %s'%(new_branch_name, FULL_PATH)
+                    new_branch = local_repo.create_head(new_branch_name)
+                    local_repo.heads.typofix.checkout()
+    
+                    #this is just because README files can have random file names/extensions
+                    api_response = requests.get('https://api.github.com/repos/' + repo_name + '/readme')
+    
+    
+                    repoInfo = api_response.json()
+    
+                    readmeFile = repoInfo['name']
+    
+                    pathToReadmeFile = FULL_PATH + '/' + readmeFile
+    
+                    print 'found readme at {}'.format(pathToReadmeFile)
+    
+                    newFileArray = []
     
                     try:
-                        result.create_pull(title='typo fix', body='fix simple typo', base='master', head='lpmi-13:{}'.format(new_branch_name))
+                        with open(pathToReadmeFile) as readme:
+                            for line in readme:
+                                newFileArray.append(line)
+                        #this is where the loop through all auxiliary forms happens
+                        revised_array = parsefile(newFileArray, be_aux_forms, FORM, CORRECTION_FORM)
+    
+                        #this takes the full updated readme and writes it to the file
+                        with open(pathToReadmeFile, 'w') as new_readme:
+                            for line in revised_array:
+                                new_readme.write(line)
+    
+                        print 'adding new README and committing to the ' + new_branch_name + ' branch'
+    
+                        updatedReadmePath = os.path.join(local_repo.working_tree_dir, readmeFile)
+    
+                        local_repo.index.add([updatedReadmePath])
+                        local_repo.index.commit('fix simple typo')
+    
+    
+                        print 'pushing back changes to remote fork'
+                        try:
+        
+                            origin.push(refspec='{}'.format(new_branch_name))
+                        except:
+        
+                            local_repo.remotes.origin.pull(refspec='{}'.format(default_branch))
+                            origin.push(refspec='{}'.format(new_branch_name))
+                        print 'creating pull request from new branch'
+    
+                        #attempts to submit a pull request for the changes
+                        try:
+                            pull_request = result.create_pull(title='typo fix', body='fix simple typo', base='{}'.format(default_branch), head='lpmi-13:{}'.format(new_branch_name))
+                            #if successful, updates the record in the database
+                            record_to_update = db.pypo.update_one({'repo_name': item['repo_name']}, { '$set' : { 'pull_request_submitted' : True ,  'pull_request_number' : pull_request.number}})
+    
+                        except:
+                            import pdb;pdb.set_trace()
+        
+                        print 'README has been updated!'
+        
+    #                    print 'Test Complete'
+                        print '\n\n\n'
+        
+        
                     except:
-                        import pdb;pdb.set_trace()
-    
-                    print 'README has been updated!'
-    #                '''
-    
-    #                print 'Test Complete'
-                    print '\n\n\n'
-    
+                        print 'unable to find/update readme'
+                        continue
     
                 except:
-                    print 'unable to find/update readme'
+                    print 'unable to clone repo'
                     continue
-
-            except:
-                print 'unable to clone repo'
-                continue
 else:
     print 'okay, nevermind'
--- search.py
+++ search.py
@@ -2,40 +2,52 @@ from github import Github
 import requests, base64, re, yaml
 from pymongo import MongoClient
 
-with open("settings.yaml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+search_text = raw_input('What would you like to search for?')
 
-db = MongoClient().pypo
+if search_text != '':
+    
+    with open("settings.yaml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+    
+    db = MongoClient().pypo
+    
+    g = Github(cfg['user'], cfg['password'], timeout=200, per_page=30)
+    
+    results = g.search_repositories('"{}+ "+in:readme'.format(search_text))
+    
+    
+    print 'there are %d total results' % results.totalCount
+    
+    string_match = '{}'.format(search_text)
+    
+    regex_string = '^.*%s\s.*$' % (string_match)
+    
+    for repo in results:
+        r = requests.get(repo.html_url)
+        if r.headers.get('status') is not int(404):
+            print 'url is ', repo.html_url
+    
+            readme = repo.get_readme()
+            data = base64.b64decode(readme.content)
+    
+            for line in data.splitlines():
+                for item in re.findall(regex_string, line):
+                    print item
+                    result = db.pypo.insert_one({
+                        "url": repo.html_url,
+                        "repo_id": repo.id,
+                        "repo_name": repo.full_name,
+                        "pull_request_submitted": False,
+                        "pull_request_merged": False,
+                        "pull_request_number": 0,
+                        "search_text": search_text,
+                        "line": item,
+                        "fullText": data
+                    })
+                    print "Saved [{}].".format(result.inserted_id)
+            print ('\n\n\n\n')
+    
+    print '\n\n\nall done!'
 
-g = Github(cfg['user'], cfg['password'], timeout=200, per_page=30)
-
-results = g.search_repositories('"is+spend+ "+in:readme')
-
-
-print 'there are %d total results' % results.totalCount
-
-string_match = 'is spend'
-
-regex_string = '^.*%s\s.*$' % (string_match)
-
-for repo in results:
-    r = requests.get(repo.html_url)
-    if r.headers.get('status') is not int(404):
-        print 'url is ', repo.html_url
-
-        readme = repo.get_readme()
-        data = base64.b64decode(readme.content)
-
-        for line in data.splitlines():
-            for item in re.findall(regex_string, line):
-                print item
-                result = db.pypo.insert_one({
-                    "url": repo.html_url,
-                    "repo_id": repo.id,
-                    "line": item,
-                    "fullText": data
-                })
-                print "Saved [{}].".format(result.inserted_id)
-        print ('\n\n\n\n')
-
-print '\n\n\nall done!'
+else:
+    print 'alright, nevermind then...'
